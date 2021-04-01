aadfab12194fc37a635038c82dac15206648839f
Kian <kian@kian>
2021-04-01 15:34:00 -0700

    commit message

---
+++ .vimrc
@@ -0,0 +1,3 @@
+" js uses 2 spaces in this project
+autocmd BufRead,BufNewFile *.js setlocal tabstop=2 shiftwidth=2 softtabstop=2 smartindent
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <section>
+        <header>30 cards</header>
+        <p>0x123...456</p>
+        <p>0x123...456</p>
+        <p>0x123...456</p>
+      </section>
+
+      <aside>
+        <img src="https://via.placeholder.com/150x200" />
+      </aside>
+
+
     </div>
   );
 }
